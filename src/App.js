2dc4a354aac31b70cdc687a1fbb74403bf4464a7
dianagatcan <gatcan.diana@gmail.com>
2022-05-25 16:46:36 +0300

    pass city to the parrent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Insert from "./Insert";
 
 
 
 export default function App(){
+
+    const[city, setCity] = useState('')
+
+    function saveCity(data){
+        setCity(data);
+    }
+
+
     return(
         <div>
-            <h1>Hello</h1>
-            <Insert />
+            <h1>{city}</h1>
+            {/* Get city from Insert through passCity */}
+            <Insert passCity={saveCity} />
         </div>
     )
 
--- src/Insert.jsx
+++ src/Insert.jsx
@@ -1,27 +1,28 @@
 import React, {useState} from "react";
 
 export default function Insert(props){
-    const [city, setCity] = useState('');
+    const [input, setInput] = useState('');
 
     function handleKeyDown(event){
         //When the user press Enter key:
         // - set 'city' as a parameter for the function received from the parrent - App
-        // - reset the city
+        // - reset the input and clear Input text
         if(event.key === "Enter"){
-            props.saveCity(city);
-            setCity('');
+            props.passCity(input);
+            setInput('');
         }
 
     }
 
     //setCity every time when user is typing
     function handleChange(event){
-        setCity(event.target.value)
+        setInput(event.target.value)
     }
 
 
     return(<div>
         <input 
+        value={input}
         type="text"
         onChange={handleChange}
         onKeyDown={(e) => handleKeyDown(e)}
