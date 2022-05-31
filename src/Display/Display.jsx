4c1c87a9b6334b8ba839da60bdfc55f4c30ececc
dianagatcan <gatcan.diana@gmail.com>
2022-05-31 17:32:06 +0300

    add forecast details

--- src/Display/Display.jsx
+++ src/Display/Display.jsx
@@ -1,21 +1,37 @@
-import React from "react";
-import './styles.css'
+import React, { useState } from "react";
+import Forecast from "../Forecast/Forecast";
+import './styles.css';
 
 export default function Display(props){
+    const api = process.env.REACT_APP_API;
+
+    const lat = props.data?.coord?.lat
+    const lon = props.data?.coord?.lon
+    const exclude = 'current,minutely,hourly,alerts'
+    const [forecast, setForecast] = useState('')
+    const [displayForecast, setDisplayForecast] = useState(false);
+
+    async function handleClick(){
+        const request = new Request(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${api}&units=metric`)
+        const jsonResponse = await (await fetch(request)).json();
+        setForecast(jsonResponse.daily.slice(0,4))
+        setDisplayForecast(true)
+    }
 
 
-    return(<div className="displayContainer">
-        <div className="temperature">
-            <span>{props.data?.main?.temp}° </span>
-            <p className="bold">{props.data?.name}</p>
+    return(<div className="main">
+        <div className="displayContainer">
+            <div className="temperature">
+                <span>{props.data?.main?.temp}°C </span>
+                <p className="bold">{props.data?.name}</p>
+            </div>
+            <div className="description">
+                <img src={`http://openweathermap.org/img/wn/${props.data?.weather?.[0].icon}@2x.png`} />
+                <p className="weatherDescription">{props.data?.weather?.[0].description}</p>
+            </div>
         </div>
-        <div className="description">
-            <img src={`http://openweathermap.org/img/wn/${props.data?.weather?.[0].icon}@2x.png`} />
-            <p className="weatherDescription">{props.data?.weather?.[0].description}</p>
-        </div>
-        {/* <p>Humidity: {props.data?.main?.humidity}% </p>
-        <p>Wind speed: {props.data?.wind?.speed} miles/hour </p> */}
+        <button onClick={handleClick}>Weather Forecast</button>
+        {displayForecast ? <Forecast data={forecast} /> : <></>}
     </div>)
-
 }
 
---
+++ src/Forecast/Forecast.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.css';
+
+export default function Forecast(props){
+
+    return(<div className="row">
+    {props.data.map((day, i) => <div className="card" key={i}>
+        <p>{new Date(props.data?.[i].dt * 1000).toLocaleDateString('en-us', { weekday:"short", month:"long", day:"numeric"})}</p>
+        <img src={`http://openweathermap.org/img/wn/${props.data?.[i].weather?.[0].icon}@2x.png`} />
+        <p>{props.data?.[i].weather?.[0].description}</p>
+        <p>{props.data?.[i].temp?.day}°C</p>
+    </div>)}
+    </div>)
+}
