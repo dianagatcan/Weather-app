95b49263ef696de4a64ad9f97a15fd986394cf99
dianagatcan <gatcan.diana@gmail.com>
2022-06-01 11:23:20 +0300

    new styles

--- src/Forecast/Forecast.jsx
+++ src/Forecast/Forecast.jsx
@@ -5,7 +5,7 @@ export default function Forecast(props){
 
     return(<div className="row">
     {props.data.map((day, i) => <div className="card" key={i}>
-        <p>{new Date(props.data?.[i].dt * 1000).toLocaleDateString('en-us', { weekday:"short", month:"long", day:"numeric"})}</p>
+        <p className="date">{new Date(props.data?.[i].dt * 1000).toLocaleDateString('en-us', { weekday:"short", month:"long", day:"numeric"})}</p>
         <img src={`http://openweathermap.org/img/wn/${props.data?.[i].weather?.[0].icon}@2x.png`} />
         <p>{props.data?.[i].weather?.[0].description}</p>
         <p>{props.data?.[i].temp?.day}°C</p>
